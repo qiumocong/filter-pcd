a11dbd9f8b5cd250dbc214f430387ac2a6b26837
mocongqiu <mocongqiu@gmail.com>
2025-12-23 11:33:31 +0800

    test import ply to rviz

--- add_ply_to_rviz.py
+++ add_ply_to_rviz.py
@@ -1,157 +1,55 @@
-import sys
-import os
+#!/usr/bin/env python3
 
-import rclpy
-from rclpy.node import Node
-import sensor_msgs.msg as sensor_msgs
-
-import numpy as np
-import open3d as o3d
-
-
-class PCDListener(Node):
-    def __init__(self):
-        super().__init__('pcd_subsriber_node')
-
-        ## This is for visualization of the received point cloud.
-        self.vis = o3d.visualization.Visualizer()
-        self.vis.create_window()
-        self.o3d_pcd = o3d.geometry.PointCloud()
-
-        # Set up a subscription to the 'pcd' topic with a callback to the
-        # function `listener_callback`
-        self.pcd_subscriber = self.create_subscription(
-            sensor_msgs.PointCloud2,  # Msg type
-            'pcd',  # topic
-            self.listener_callback,  # Function to call
-            10  # QoS
-        )
-
-    def listener_callback(self, msg):
-        # Here we convert the 'msg', which is of the type PointCloud2.
-        # I ported the function read_points2 from
-        # the ROS1 package.
-        # https://github.com/ros/common_msgs/blob/noetic-devel/sensor_msgs/src/sensor_msgs/point_cloud2.py
-
-        pcd_as_numpy_array = np.array(list(read_points(msg)))
-
-        # The rest here is for visualization.
-        self.vis.remove_geometry(self.o3d_pcd)
-        self.o3d_pcd = o3d.geometry.PointCloud(
-            o3d.utility.Vector3dVector(pcd_as_numpy_array))
-
-        self.vis.add_geometry(self.o3d_pcd)
-
-        self.vis.poll_events()
-        self.vis.update_renderer()
-
-
-## The code below is "ported" from
-# https://github.com/ros/common_msgs/tree/noetic-devel/sensor_msgs/src/sensor_msgs
-import sys
-from collections import namedtuple
-import ctypes
-import math
-import struct
+import rospy
+import std_msgs.msg
 from sensor_msgs.msg import PointCloud2, PointField
-
-_DATATYPES = {}
-_DATATYPES[PointField.INT8] = ('b', 1)
-_DATATYPES[PointField.UINT8] = ('B', 1)
-_DATATYPES[PointField.INT16] = ('h', 2)
-_DATATYPES[PointField.UINT16] = ('H', 2)
-_DATATYPES[PointField.INT32] = ('i', 4)
-_DATATYPES[PointField.UINT32] = ('I', 4)
-_DATATYPES[PointField.FLOAT32] = ('f', 4)
-_DATATYPES[PointField.FLOAT64] = ('d', 8)
+import sensor_msgs.point_cloud2 as pc2
+import numpy as np
+import math
 
 
-def read_points(cloud, field_names=None, skip_nans=False, uvs=[]):
-    """
-    Read points from a L{sensor_msgs.PointCloud2} message.
+def main():
+    # 1. 初始化 ROS 节点
+    rospy.init_node('point_cloud_publisher_node')
 
-    @param cloud: The point cloud to read from.
-    @type  cloud: L{sensor_msgs.PointCloud2}
-    @param field_names: The names of fields to read. If None, read all fields. [default: None]
-    @type  field_names: iterable
-    @param skip_nans: If True, then don't return any point with a NaN value.
-    @type  skip_nans: bool [default: False]
-    @param uvs: If specified, then only return the points at the given coordinates. [default: empty list]
-    @type  uvs: iterable
-    @return: Generator which yields a list of values for each point.
-    @rtype:  generator
-    """
-    assert isinstance(cloud, PointCloud2), 'cloud is not a sensor_msgs.msg.PointCloud2'
-    fmt = _get_struct_fmt(cloud.is_bigendian, cloud.fields, field_names)
-    width, height, point_step, row_step, data, isnan = cloud.width, cloud.height, cloud.point_step, cloud.row_step, cloud.data, math.isnan
-    unpack_from = struct.Struct(fmt).unpack_from
+    # 2. 创建发布者 (Topic 名称: "/my_point_cloud")
+    pub = rospy.Publisher('/my_point_cloud', PointCloud2, queue_size=10)
 
-    if skip_nans:
-        if uvs:
-            for u, v in uvs:
-                p = unpack_from(data, (row_step * v) + (point_step * u))
-                has_nan = False
-                for pv in p:
-                    if isnan(pv):
-                        has_nan = True
-                        break
-                if not has_nan:
-                    yield p
-        else:
-            for v in range(height):
-                offset = row_step * v
-                for u in range(width):
-                    p = unpack_from(data, offset)
-                    has_nan = False
-                    for pv in p:
-                        if isnan(pv):
-                            has_nan = True
-                            break
-                    if not has_nan:
-                        yield p
-                    offset += point_step
-    else:
-        if uvs:
-            for u, v in uvs:
-                yield unpack_from(data, (row_step * v) + (point_step * u))
-        else:
-            for v in range(height):
-                offset = row_step * v
-                for u in range(width):
-                    yield unpack_from(data, offset)
-                    offset += point_step
+    # 设置发布频率 (10Hz)
+    rate = rospy.Rate(10)
 
+    rospy.loginfo("开始发布点云数据...")
 
-def _get_struct_fmt(is_bigendian, fields, field_names=None):
-    fmt = '>' if is_bigendian else '<'
+    while not rospy.is_shutdown():
+        # 3. 模拟实时数据生成 (例如生成一个旋转的正弦波点云)
+        # 实际使用时，这里替换为你的真实点云数据源
+        points = []
+        t = rospy.get_time()
+        for i in range(1000):
+            x = float(i) / 100.0
+            y = math.sin(x + t)  # 让波形动起来
+            z = math.cos(x + t)
 
-    offset = 0
-    for field in (f for f in sorted(fields, key=lambda f: f.offset) if field_names is None or f.name in field_names):
-        if offset < field.offset:
-            fmt += 'x' * (field.offset - offset)
-            offset = field.offset
-        if field.datatype not in _DATATYPES:
-            print('Skipping unknown PointField datatype [%d]' % field.datatype, file=sys.stderr)
-        else:
-            datatype_fmt, datatype_length = _DATATYPES[field.datatype]
-            fmt += field.count * datatype_fmt
-            offset += field.count * datatype_length
+            # (x, y, z) 坐标
+            points.append([x, y, z])
 
-    return fmt
+        # 4. 创建 PointCloud2 消息头
+        header = std_msgs.msg.Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = "map"  # 重要：RViz 中 Fixed Frame 需要设置为这个值
 
+        # 5. 生成 PointCloud2 消息
+        # create_cloud_xyz32 用于仅包含 XYZ 的简单点云
+        pc_msg = pc2.create_cloud_xyz32(header, points)
 
-def main(args=None):
-    # Boilerplate code.
-    rclpy.init(args=args)
-    pcd_listener = PCDListener()
-    rclpy.spin(pcd_listener)
+        # 6. 发布消息
+        pub.publish(pc_msg)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    pcd_listener.destroy_node()
-    rclpy.shutdown()
+        rate.sleep()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
